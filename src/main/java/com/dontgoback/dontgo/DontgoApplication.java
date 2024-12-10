eca4c7f35e3b3a85cc1b73247e5c75b7317ae10d
parkhongseok <123qkrghdtjr@naver.com>
2024-12-10 15:09:06 +0900

    프로젝트 생성 완료2

---
+++ src/main/java/com/dontgoback/dontgo/DontgoApplication.java
@@ -0,0 +1,13 @@
+package com.dontgoback.dontgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontgoApplication.class, args);
+	}
+
+}
