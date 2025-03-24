9ab964f8079d4742c071182c298d6fce08ca668c
parkhongseok <123qkrghdtjr@naver.com>
2025-03-24 16:40:10 +0900

    Principal & Token

--- frontend_v2/src/app/(layout)/utils/globalValues.tsx
+++ frontend_v2/src/app/(layout)/utils/globalValues.tsx
@@ -2,9 +2,7 @@ export const MAX_TEXT_LENGTH = 500;
 export const ACCESS_TOKEN_NAME = "access_token";
 export const REFRESH_TOKEN_NAME = "refresh_token";
 
-export const ACCESS_TOKEN_FOR_VISITER =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDE1OTg4MTcsImV4cCI6MTc0MTY4NTIxNywic3ViIjoiMTIzcWtyZ2hkdGpyQGdtYWlsLmNvbSIsImlkIjoxfQ.URrRvXaSXjJKt1m8ny5Qh_mQ3GSKc9KlgsZdJc8Y16M";
-
+export const ACCESS_TOKEN_FOR_VISITER = process.env.NEXT_PUBLIC_JWT;
 // 로컬
 export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL!;
 export const BACKEND_API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL!;
