c987c21101a1b8109d3de19e780c87def8431f9a
parkhongseok <123qkrghdtjr@naver.com>
2025-08-02 18:30:02 +0900

    feat: Assethistory 엔티티 추가

---
+++ dontgo/src/main/java/com/dontgoback/dontgo/domain/user/AccountCreateService.java
@@ -0,0 +1,4 @@
+package com.dontgoback.dontgo.domain.user;
+
+public class AccountCreateService {
+}
---
+++ dontgo/src/main/java/com/dontgoback/dontgo/global/initData/CreateDummyUser.java
@@ -0,0 +1,4 @@
+package com.dontgoback.dontgo.global.initData;
+
+public class CreateDummyUser {
+}
