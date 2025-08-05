bb3c55f0e48ece77e38a956b5a90226a9257975e
parkhongseok <123qkrghdtjr@naver.com>
2025-08-05 19:29:48 +0900

    rename modules: frontend_v2 → frontend, dontgo → dg-core-server

--- dg-core-server/src/main/java/com/dontgoback/dontgo/DgCoreServerApplication.java
+++ dg-core-server/src/main/java/com/dontgoback/dontgo/DgCoreServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class DontgoApplication {
+public class DgCoreServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DontgoApplication.class, args);
+		SpringApplication.run(DgCoreServerApplication.class, args);
 	}
 
 }
