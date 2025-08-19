a08c7afcd28bbd35e7139315c672d9d139e03bcd
parkhongseok <123qkrghdtjr@naver.com>
2025-08-19 19:55:52 +0900

    feat: logout refresh cookie delete

--- dg-core-server/src/main/java/com/dontgoback/dontgo/domain/refreshToken/TokenCookieService.java
+++ dg-core-server/src/main/java/com/dontgoback/dontgo/domain/refreshToken/TokenCookieService.java
@@ -12,8 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.time.Duration;
 
-import static com.dontgoback.dontgo.global.util.GlobalValues.ACCESS_TOKEN_NAME;
-import static com.dontgoback.dontgo.global.util.GlobalValues.REFRESH_TOKEN_NAME;
+import static com.dontgoback.dontgo.global.util.GlobalValues.*;
 
 @RequiredArgsConstructor
 @Service
@@ -28,7 +27,6 @@ public class TokenCookieService {
 
     // 상수 정의
     private static final String ROOT_PATH = "/";
-    private static final String REFRESH_TOKEN_API_PATH = "/api/token"; // 이전 TokenApiController 경로와 일치
     private static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     private static final Duration ACCESS_TOKEN_DURATION = Duration.ofHours(2); // TokenService의 만료시간과 일치
 
@@ -53,7 +51,7 @@ public class TokenCookieService {
 
     // 필요 시 로그아웃 등 쿠키 삭제 메서드도 추가 가능
     public void clearTokenCookies(HttpServletRequest request, HttpServletResponse response) {
-        CookieUtil.deleteCookie(request, response, ACCESS_TOKEN_NAME, secureCookie);
-        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_NAME, secureCookie);
+        CookieUtil.deleteCookie(request, response, ACCESS_TOKEN_NAME, "/", secureCookie);
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_NAME, REFRESH_TOKEN_API_PATH, secureCookie);
     }
 }
